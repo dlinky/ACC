60254dae5b5d4b9c0f5e5b3b39c006651f2bc35f
dlinky <prooding@gmail.com>
2021-12-07 13:17:59 +0900

    watershed 추가했으나 제대로 적용되지 않음.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import shutil
 import glob
@@ -8,6 +9,7 @@ import time
 import sys
 import argparse
 import remove_scope
+from matplotlib import pyplot as plt
 
 current_filename = ''
 
@@ -25,6 +27,12 @@ SAVE_IMAGE = args.save_images
 RUN_TIME = args.run_time
 REMOVE_SCOPE = args.remove_scope
 
+# constants
+PLATELET = 0
+RBC = 1
+WBC = 2
+UNKNOWN = 3
+
 # paths
 INPUT_PATH = os.path.join(os.getcwd(), 'input')
 OUTPUT_PATH = os.path.join(os.getcwd(), 'output')
@@ -33,11 +41,16 @@ THRESHOLD_PATH = os.path.join(OUTPUT_PATH, 'threshold')
 LABEL_PATH = os.path.join(OUTPUT_PATH, 'labeled')
 BBOX_PATH = os.path.join(OUTPUT_PATH, 'bbox')
 CLASSES_PATH = os.path.join(OUTPUT_PATH, 'bbox', 'classes')
+WATERSHED_PATH = os.path.join(OUTPUT_PATH, 'labeled', 'watershed')
 
 
 def error(msg):
     print(msg)
     sys.exit(0)
+
+
+def empty(self):
+    pass
 
 
 class Clock:
@@ -112,11 +125,10 @@ def threshold(src):
 
 def label_cells(threshed):
     _, labels, stats, _ = cv2.connectedComponentsWithStats(threshed)
-    labeled_stats = []
-    for box in stats:
-        labeled_stats.append([box[0], box[1], box[2], box[3], box[4], 'Undefined'])
-
-    return labels, labeled_stats[1:]
+    label = np.full((len(stats), 1), UNKNOWN, np.uint8)
+    idx = np.array(range(len(stats)), np.uint8).reshape(len(stats),1)
+    stats = np.concatenate((stats, idx, label), axis=1)
+    return labels, stats[1:]
 
 
 def cvt_label_to_image(labels):
@@ -140,41 +152,144 @@ def cvt_label_to_image(labels):
 def remove_border_touching_bbox(labels, stats, edge_error):
     height = len(labels)
     width = len(labels[0])
-    new_stats = []
-    for idx, box in enumerate(stats[:]):
+    remove_stats = []
+    for idx, box in enumerate(stats):
         lboard = box[0] < edge_error
         rboard = box[0] + box[2] > height - edge_error
         tboard = box[1] < edge_error
         bboard = box[1] + box[3] > width - edge_error
         if lboard or rboard or tboard or bboard:
-            labels[labels == idx] = 0
-        else:
-            new_stats.append(box)
-    return labels, new_stats
+            labels[labels == idx+1] = 0
+            remove_stats.append(idx)
+    stats = np.delete(stats, remove_stats, axis=0)
+    cv2.waitKey(0)
+    return labels, stats[1:]
 
 
-def double_threshold(img):
+def double_threshold(roi):
     """
     process otsu threshold twice with a
-    :param img: 
+    :param roi:
     :return: 
     """
-    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    lab = cv2.cvtColor(roi, cv2.COLOR_BGR2LAB)
     l, a, b = cv2.split(lab)
     ret_cell, th1_a = cv2.threshold(a, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_TOZERO)
     th1_a[th1_a == 0] = ret_cell
     ret_nucleus, th2_a = cv2.threshold(th1_a, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+
     return ret_cell, ret_nucleus
 
 
 def find_wbc(img, stats):
     wbcs = 0
     for box in stats:
-        ret_cell, ret_nucleus = double_threshold(img[box[1]:box[1]+box[3], box[0]:box[0]+box[2]])
+        roi = img[box[1]:box[1]+box[3], box[0]:box[0]+box[2]]
+        ret_cell, ret_nucleus = double_threshold(roi)
         if ret_nucleus - ret_cell > 10:
-            box[-1] = 'WBC'
-            wbcs = 1
+            box[-1] = WBC
+            wbcs += 1
+            if SAVE_IMAGE:
+                a = cv2.split(cv2.cvtColor(roi, cv2.COLOR_BGR2LAB))[1]
+                _, mask_cell = cv2.threshold(a, ret_cell, 255, cv2.THRESH_TOZERO)
+                masked_cell = cv2.bitwise_and(roi, roi, mask=mask_cell)
+                _, mask_nucleus = cv2.threshold(a, ret_nucleus, 255, cv2.THRESH_TOZERO)
+                masked_nucleus = cv2.bitwise_and(roi, roi, mask=mask_nucleus)
+                masked_img = cv2.addWeighted(masked_cell, 0.5, masked_nucleus, 0.5, 0)
+                cv2.imwrite(os.path.join(CLASSES_PATH, current_filename.replace('.jpg', f'-wbc{wbcs}.jpg')), masked_img)
     return wbcs, stats
+
+
+def watershed(labels, box, img):
+    rmin = int(max(box[1] - 0.5 * box[3], 0))
+    rmax = int(min(box[1] + 1.5 * box[3], len(labels)))
+    cmin = int(max(box[0] - 0.5 * box[2], 0))
+    cmax = int(min(box[0] + 1.5 * box[2], len(labels[0])))
+    box_label = box[-2]
+    roi = img.copy()[rmin:rmax, cmin:cmax]
+    a = cv2.split(cv2.cvtColor(roi, cv2.COLOR_BGR2LAB))[1]
+    mask = labels.copy()[rmin:rmax, cmin:cmax]
+    mask[mask != box_label] = 0
+    mask[mask == box_label] = 255
+    mask = np.uint8(mask)
+    masked = cv2.bitwise_and(a, a, mask=mask)
+    _, threshed = cv2.threshold(masked, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+    kernel = np.ones((3,3))
+    opening = cv2.morphologyEx(threshed, cv2.MORPH_OPEN, kernel, iterations=2)
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    _, sure_fg = cv2.threshold(dist_transform, 0.5*dist_transform.max(), 255, 0)
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    _, markers = cv2.connectedComponents(sure_fg)
+    markers = markers + 1
+    markers[unknown == 255] = 0
+    markers = cv2.watershed(roi, markers)
+    markers[markers == -1] = 0
+    markers[markers == 1] = 0
+    markers = cv2.morphologyEx(np.uint8(markers), cv2.MORPH_OPEN, kernel)
+    _, _, stats, _ = cv2.connectedComponentsWithStats(np.uint8(markers), connectivity=8)
+
+    label = np.full((len(stats), 1), UNKNOWN, np.uint8)
+    idx = np.array(range(len(stats+box[5])), np.uint8).reshape(len(stats), 1)
+    stats = np.concatenate((stats, idx, label), axis=1)
+    for idx, stat in enumerate(stats[:]):
+        if stat[0] == 0:
+            stats = np.delete(stats, idx, axis=0)
+    print(stats)
+    for stat in stats:
+        cv2.rectangle(roi, (stat[0], stat[1]), (stat[0] + stat[2], stat[1] + stat[3]), (0, 0, 255), 2)
+    stats += np.array([box[0], box[1], 0, 0, 0, box[5], 0])
+
+    images = [masked, threshed, sure_bg, dist_transform, sure_fg, unknown, markers, roi]
+    titles = ['Gray', 'Threshed', 'Sure BG', 'Distance', 'Sure FG', 'Unknown', 'Markers', 'Result']
+
+    for i in range(len(images)):
+        plt.subplot(2, 4, i + 1), plt.imshow(images[i]), plt.title(titles[i]), plt.xticks([]), plt.yticks([])
+    plt.show()
+    return stats
+
+
+def separate_with_circle(img, box):
+    roi = img[box[1]-2:box[1] + box[3]+2, box[0]-2:box[0] + box[2]+2]
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    windowName = 'separate'
+    cv2.namedWindow(windowName)
+    cv2.createTrackbar('mindist', windowName, 1, 100, empty)
+    cv2.createTrackbar('param1', windowName, 1, 255, empty)
+    cv2.createTrackbar('param2', windowName, 1, 255, empty)
+    cv2.createTrackbar('minradius', windowName, 1, 255, empty)
+    cv2.createTrackbar('maxradius', windowName, 1, 255, empty)
+
+    while True:
+        temp = copy.deepcopy(roi)
+        if cv2.waitKey(10) == 27:
+            break
+        minDist = cv2.getTrackbarPos('mindist', windowName)
+        param1 = cv2.getTrackbarPos('param1', windowName)
+        param2 = cv2.getTrackbarPos('param2', windowName)
+        minRadius = cv2.getTrackbarPos('minradius', windowName)
+        maxRadius = cv2.getTrackbarPos('maxradius', windowName)
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, minDist=minDist+1, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+        if circles is not None:
+            for i in circles[0,:]:
+                cv2.circle(temp, (i[0], i[1]), i[2], (0, 255, 0), 2)
+
+        cv2.resizeWindow(windowName, 500, 1000)
+        cv2.imshow(windowName, temp)
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, minDist=minDist, param1=param1, param2=param2,
+                               minRadius=minRadius, maxRadius=maxRadius)
+    if circles is not None:
+        new_box = np.zeros((1, 6))
+        for i in circles[0,:]:
+            x_min = int(i[0]-i[2])
+            y_min = int(i[1]-i[2])
+            new_box = np.append(new_box, [x_min, y_min, int(2*i[2]), int(2*i[2]), int(2*3.14*i[2]**2)])
+        return new_box
+    else:
+        return box
 
 
 def cluster_boxes(num_clst, stats):
@@ -201,13 +316,13 @@ def cluster_boxes(num_clst, stats):
         for i in range(num_clst):
             clst_mu[i] = np.mean(data[np.where(clst == i)[0]])
 
-    label_list = ['Platelets', 'RBC', 'Connected RBC', 'WBC']  # 일반 RBC, 덩어리진 RBC
+    label_list = [PLATELET, RBC, WBC, UNKNOWN]  # 일반 RBC, 덩어리진 RBC
     for idx, label_idx in enumerate(clst):
         if idx == 3:
-            if stats[idx][-1] == 'WBC':
+            if stats[idx][-1] == WBC:
                 pass
             else:
-                stats[idx][-1] = 'Connected RBC'
+                stats[idx][-1] = RBC
         else:
             stats[idx][-1] = label_list[label_idx]
     return stats
@@ -221,13 +336,13 @@ def classify_boxes(num_clst, stats):
 def cvt_stat(stats):
     bboxes = []
     for line in stats:
-        bboxes.append([line[5], line[0], line[1], line[0]+line[2], line[1]+line[3]])
+        bboxes.append([line[-1], line[0], line[1], line[0]+line[2], line[1]+line[3]])
     return bboxes
 
 
 def print_box(boxes, img):
-    names = ['WBC', 'Connected RBC', 'RBC', 'Platelets', 'Undefined']
-    colors = [(255, 255, 255), (100, 100, 255), (0, 0, 255), (255, 0, 0), (100, 100, 100)]
+    names = [WBC, RBC, PLATELET, UNKNOWN]
+    colors = [(255, 255, 255), (0, 0, 255), (255, 0, 0), (50, 50, 50)]
     for i, item in enumerate(boxes):
         color = colors[names.index(item[0])]
         xmin = item[1]
@@ -239,6 +354,7 @@ def print_box(boxes, img):
 
 
 def write_xml(img, table):
+    names = ['Platelets', 'RBC', 'WBC', 'Unknown']
     root = ET.Element('annotation')
     ET.SubElement(root, 'folder').text = os.path.dirname(OUTPUT_PATH)
     ET.SubElement(root, 'filename').text = current_filename
@@ -256,7 +372,7 @@ def write_xml(img, table):
 
     for line in table:
         obj = ET.SubElement(root, 'object')
-        ET.SubElement(obj, 'name').text = line[0]
+        ET.SubElement(obj, 'name').text = names[line[0]]
         ET.SubElement(obj, 'pose').text = 'Unspecified'
         ET.SubElement(obj, 'truncated').text = '0'
         ET.SubElement(obj, 'difficult').text = '0'
@@ -268,7 +384,7 @@ def write_xml(img, table):
         ET.SubElement(bndbox, 'ymax').text = str(line[4])
 
     tree = ET.ElementTree(root)
-    tree.write(os.path.join(XML_PATH, current_filename))
+    tree.write(os.path.join(XML_PATH, current_filename.replace('.jpg', '.xml')))
 
 
 def print_run_time():
@@ -301,13 +417,15 @@ def main():
         os.makedirs(THRESHOLD_PATH)
         os.makedirs(LABEL_PATH)
         os.makedirs(BBOX_PATH)
+        os.makedirs(CLASSES_PATH)
+        os.makedirs(WATERSHED_PATH)
 
     file_list = glob.glob(INPUT_PATH + '/*.jpg')
     if len(file_list) == 0:
         error("Error: No input files found")
-    for idx, filename in enumerate(file_list):
+    for page, filename in enumerate(file_list):
         clk_total.start()
-        print_progress(idx, len(file_list))
+        print_progress(page, len(file_list))
         global current_filename
         current_filename = os.path.basename(filename)
 
@@ -318,10 +436,24 @@ def main():
                 continue
             else:
                 img = cropped_img
+
         clk_detect.start()
         threshed = threshold(img)
         labels, stats = label_cells(threshed)
         labels, stats = remove_border_touching_bbox(labels, stats, 1)
+
+        for idx, stat in enumerate(stats):
+            new_stats = watershed(labels, stat, img)
+            # new_stats = separate_with_circle(img, stat)
+            if new_stats.ndim == 2 and len(new_stats) > 1:
+                if SAVE_IMAGE:
+                    temp = copy.deepcopy(img)
+                    for new_stat in new_stats:
+                        cv2.rectangle(temp, (new_stat[0], new_stat[1]), (new_stat[0]+new_stat[2], new_stat[1]+new_stat[3]), (0, 255, 0), 2)
+                    roi = temp[stat[1]:stat[1] + stat[3], stat[0]:stat[0] + stat[2]]
+                    cv2.imwrite(os.path.join(WATERSHED_PATH, current_filename.replace('.jpg', f'-{idx+1}({len(new_stats)}).jpg')), roi)
+                stats = np.delete(stats, idx, axis=0)
+                stats = np.append(stats, tuple(new_stats), axis=0)
         clk_detect.stop()
         
         clk_classify.start()
